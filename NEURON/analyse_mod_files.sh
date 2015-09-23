56c6f39e442bd72f1e327fd94a791338b20a3638
Padraig Gleeson <p.gleeson@gmail.com>
2015-09-23 16:05:50 +0100

    Adding scripts for analysing/comparing channels

---
+++ NEURON/analyse_mod_files.sh
@@ -0,0 +1,4 @@
+# This script uses utilities in https://github.com/NeuroML/pyNeuroML to generate
+# plots for the activation variables, etc. of selected channels in mod files
+
+pynml-modchananalysis Golgi_NaP -stepV 5 -temperature 23 
---
+++ neuroConstruct/generatedNeuroML2/analyse_chans.sh
@@ -0,0 +1,7 @@
+
+pynml-channelanalysis NaP_CML.channel.nml \
+                      -caConc 4.3e-4 -temperature 23 -html -md
+
+
+
+# Im.channel.nml  problematic...
