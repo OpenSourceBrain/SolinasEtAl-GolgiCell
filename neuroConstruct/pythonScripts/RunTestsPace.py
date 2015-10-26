734f4801299a64ee6632ee6b0fb681eef850952a
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-26 17:13:06 +0000

    Adding test comparing original NEURON version pacemaking spiketimes to nC -> Neuron version

---
+++ neuroConstruct/pythonScripts/RunTestsPace.py
@@ -0,0 +1,104 @@
+#
+#
+#   File to test current configuration of GranuleCell project.
+#
+#   To execute this type of file, type '..\..\..\nC.bat -python XXX.py' (Windows)
+#   or '../../../nC.sh -python XXX.py' (Linux/Mac). Note: you may have to update the
+#   NC_HOME and NC_MAX_MEMORY variables in nC.bat/nC.sh
+#
+#   Author: Padraig Gleeson
+#
+#   This file has been developed as part of the neuroConstruct project
+#   This work has been funded by the Medical Research Council and the
+#   Wellcome Trust
+#
+#
+
+import sys
+import os
+
+try:
+    from java.io import File
+except ImportError:
+    print "Note: this file should be run using ..\\..\\..\\nC.bat -python XXX.py' or '../../../nC.sh -python XXX.py'"
+    print "See http://www.neuroconstruct.org/docs/python.html for more details"
+    quit()
+
+sys.path.append(os.environ["NC_HOME"]+"/pythonNeuroML/nCUtils")
+
+import ncutils as nc
+
+projFile = File(os.getcwd(), "../SolinasEtAl-GolgiCell.ncx")
+
+
+##############  Main settings  ##################
+
+simConfigs = []
+
+simConfigs.append("PacemakerTest")
+
+simDt =                 0.005
+
+simulators =            ["NEURON"]
+
+numConcurrentSims =     4
+
+varTimestepNeuron =     False
+
+plotSims =              True
+plotVoltageOnly =       True
+runInBackground =       True
+analyseSims =           True
+verbose =               False
+
+#############################################
+
+
+spike_times = []
+mep_file = open('../../NEURON/.test.mep')
+for line in mep_file:
+    line = line.strip()
+    if line.startswith('spike times: ['):
+        times = line[14:-1].split(',')
+        for time in times:
+            spike_times.append(1000*float(time.strip()))
+
+def testAll(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    print "Loading project from "+ projFile.getCanonicalPath()
+
+
+    simManager = nc.SimulationManager(projFile,
+                                      numConcurrentSims,
+                                      verbose)
+
+    simManager.runMultipleSims(simConfigs =           simConfigs,
+                               simDt =                simDt,
+                               simulators =           simulators,
+                               runInBackground =      runInBackground,
+                               varTimestepNeuron =    varTimestepNeuron)
+
+    simManager.reloadSims(plotVoltageOnly =   plotVoltageOnly,
+                          plotSims =          plotSims,
+                          analyseSims =       analyseSims)
+
+
+    spikeTimesToCheck = {'Golgi_CML_0': spike_times}
+    
+    
+    spikeTimeAccuracy = 1.2 # ms in 4000 ms
+
+    report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
+                                  spikeTimeAccuracy = spikeTimeAccuracy)
+
+    print report
+
+    return report
+
+
+if __name__ == "__main__":
+    testAll()
+
+
