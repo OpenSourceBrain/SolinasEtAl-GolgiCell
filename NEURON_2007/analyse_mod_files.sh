c350dd5c06a64fdb261b4818f5dde72e40fb0334
Padraig Gleeson <p.gleeson@gmail.com>
2015-12-08 19:07:41 +0000

    Making a copy of the original Neuron code in directory that will remain unchanged (NEURON dir will be updated as required)

---
+++ NEURON_2007/analyse_mod_files.sh
@@ -0,0 +1,12 @@
+# This script uses utilities in https://github.com/NeuroML/pyNeuroML to generate
+# plots for the activation variables, etc. of selected channels in mod files
+
+pynml-modchananalysis Golgi_NaP -stepV 5 -temperature [20,23] 
+pynml-modchananalysis Golgi_Na -stepV 5 -temperature [20,23] -dt 0.001
+
+pynml-modchananalysis Golgi_KV -stepV 5 -temperature [20,23] -dt 0.001
+pynml-modchananalysis Golgi_KM -stepV 5 -temperature [20,23] 
+
+pynml-modchananalysis Golgi_Ca_HVA -stepV 5 -temperature [20,23] 
+
+#pynml-modchananalysis Golgi_hcn1 -stepV 5 -temperature [20,23] 
