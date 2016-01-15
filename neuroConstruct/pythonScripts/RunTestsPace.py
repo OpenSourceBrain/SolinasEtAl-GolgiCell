4a102f6c0f5276395ae80243fc48be21f24ed4e7
Padraig Gleeson <p.gleeson@gmail.com>
2016-01-15 16:35:54 +0000

    Testing changing ca2o to 2mM
    Related to #8

--- neuroConstruct/pythonScripts/RunTestsPace.py
+++ neuroConstruct/pythonScripts/RunTestsPace.py
@@ -37,7 +37,7 @@ simConfigs = []
 
 simConfigs.append("PacemakerTest")
 
-simDt =                 0.001
+simDt =                 0.0025
 
 simulators =            ["NEURON"]
 
@@ -88,7 +88,7 @@ def testAll(argv=None):
     spikeTimesToCheck = {'Golgi_CML_0': spike_times}
     
     
-    spikeTimeAccuracy = 2.6 # ms in 4000 ms
+    spikeTimeAccuracy = 7.2 # ms in 4000 ms
 
     report = simManager.checkSims(spikeTimesToCheck = spikeTimesToCheck,
                                   spikeTimeAccuracy = spikeTimeAccuracy)
