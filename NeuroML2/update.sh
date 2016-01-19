91f2cfe438f93fc3256e36afb715071d3baf13e0
Padraig Gleeson <p.gleeson@gmail.com>
2016-01-19 18:36:38 +0000

    Renaming channels in NeuroML2 to be consistent with Table 1 in paper

--- NeuroML2/update.sh
+++ NeuroML2/update.sh
@@ -15,19 +15,19 @@ copyNml2()
 
 
 copyNml2 'NaP_CML' 'Golgi_NaP'
-copyNml2 'NaT_CML' 'Golgi_Na'
+copyNml2 'NaT_CML' 'Golgi_NaT'
 copyNml2 'NaR_CML' 'Golgi_NaR'
 
 
-copyNml2 'KA_CML' 'Golgi_KA'
-copyNml2 'Kslow_CML' 'Golgi_KM'
 copyNml2 'KV_CML' 'Golgi_KV'
-copyNml2 'KAHP_CML' 'KAHP_CML'
+copyNml2 'KA_CML' 'Golgi_KA'
+copyNml2 'Kslow_CML' 'Golgi_Kslow'
+copyNml2 'KAHP_CML' 'Golgi_KAHP'
 
 copyNml2 'CaHVA_CML' 'Golgi_Ca_HVA'
 copyNml2 'CaLVA_CML' 'Golgi_Ca_LVA'
 
-copyNml2 'KC_CML' 'Golgi_BK'
+copyNml2 'KC_CML' 'Golgi_KC'
 
 
 copyNml2 'hcn1f_CML' 'Golgi_hcn1f'
